5644c2f17f178236bf9a88a7570dadc07432562f
Rendy-4 <rendyjuans2@gmail.com>
2025-11-18 20:23:03 +0700

    fixing tampilan

--- Assets/Script/Main Character/PlayerMovement.cs
+++ Assets/Script/Main Character/PlayerMovement.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement3D : MonoBehaviour
 {
-    public Rigidbody rb;
+    public Rigidbody2D rb;
     public float moveSpeed = 5f;
 
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveX = Input.GetAxis("Horizontal");
 
         rb.linearVelocity = new Vector3(moveX * moveSpeed, rb.linearVelocity.y, 0);
     }
