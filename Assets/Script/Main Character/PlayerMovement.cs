9c5ac7bd6d9bc1b6931c281d4aa3fb9bcdab3d8b
Rendy-4 <rendyjuans2@gmail.com>
2025-11-18 09:12:59 +0700

    commit

--- Assets/Script/Main Character/PlayerMovement.cs
+++ Assets/Script/Main Character/PlayerMovement.cs
@@ -4,36 +4,11 @@ public class PlayerMovement3D : MonoBehaviour
 {
     public Rigidbody rb;
     public float moveSpeed = 5f;
-    public float jumpForce = 7f;
-    private bool isGrounded = false;
 
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
 
         rb.linearVelocity = new Vector3(moveX * moveSpeed, rb.linearVelocity.y, 0);
-
-        /*
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-        */
     }
-
-    /* private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-    }*/
 }
