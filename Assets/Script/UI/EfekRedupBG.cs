16fbe8394117d7ff9167032b5f47990e41bce678
Rendy-4 <lianradith@gmail.com>
2025-12-10 10:50:02 +0700

    perbaikan sedikit

--- Assets/Script/UI/EfekRedupBG.cs
+++ Assets/Script/UI/EfekRedupBG.cs
@@ -28,5 +28,16 @@ public void CloseCredits()
     Creditpanel.SetActive(false);
     dimBackground.SetActive(false);
 }
+/*************  ✨ Windsurf Command ⭐  *************/
+/// <summary>
+/// Enable dim background only for redup mode
+/// </summary>
+/*******  b38fe17e-863d-45e7-8eb4-f52f9759be18  *******/
+public void SedangRedupOnly(){
+    dimBackground.SetActive(true);
+}
+public void TidakRedupOnly(){
+    dimBackground.SetActive(false);
+}
 
 }
