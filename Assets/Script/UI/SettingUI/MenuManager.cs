5ecf4b77d9db5007a72dd2edd318277fd76d7692
Rendy-4 <rendyjuans2@gmail.com>
2026-01-22 21:19:06 +0700

    make transisi untuk new dan load game

--- Assets/Script/UI/SettingUI/MenuManager.cs
+++ Assets/Script/UI/SettingUI/MenuManager.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.IO;
+using System.Collections;
 
 public class MenuManager : MonoBehaviour
 {
     [Header("Menu Buttons")]
     [SerializeField] private Button newGameButton;
     [SerializeField] private Button loadGameButton;
+
+    [Header("Transition Settings")]
+    [SerializeField] private string startText = "memulai permainan...";
+    [SerializeField] private float fadeDuration = 1f;
+    [SerializeField] private float displayTime = 2f;
 
     private void Start()
     {
@@ -24,14 +30,37 @@ public class MenuManager : MonoBehaviour
     public void OnNewGameClicked()
     {
         
-            DataPresistenceManager.instance.NewGame();
+        DataPresistenceManager.instance.NewGame();
 
-        SceneManager.LoadSceneAsync("Sekolah");
+        StartCoroutine(TransitionAndLoad("Sekolah"));
     }
 
     public void OnLoadGameClicked()
     {
-        SceneManager.LoadSceneAsync("Sekolah");
+        DataPresistenceManager.instance.LoadGame();
+
+        StartCoroutine(TransitionAndLoad("Sekolah"));
+    }
+
+    IEnumerator TransitionAndLoad(string sceneName)
+    {
+        Debug.Log("Transitioning to scene: " + sceneName);
+        if (SceneOverlayUIController.Instance != null)
+        {
+            SceneOverlayUIController.Instance.PlaySceneText(
+                startText,
+                fadeDuration,
+                displayTime
+            );
+        }
+        else 
+        {
+            Debug.LogWarning("SceneOverlayUIController instance is null. Skipping scene text display.");
+        }
+
+        yield return new WaitForSeconds(fadeDuration);
+
+        SceneManager.LoadSceneAsync(sceneName);
     }
 
     public void ToMainMenu()
@@ -45,6 +74,4 @@ public class MenuManager : MonoBehaviour
         DataPresistenceManager.instance.SaveGame();
         Application.Quit();
     }
-
-    
 }
