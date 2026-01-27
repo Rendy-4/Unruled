1b0a00bb27591efd40d895ed26745ae2ff9c5415
Zeprhy <lianradith@gmail.com>
2026-01-27 10:31:05 +0700

    Fix A LB

--- Assets/Script/InteractScript/SceneTrigger.cs
+++ Assets/Script/InteractScript/SceneTrigger.cs
@@ -6,10 +6,6 @@ public class SceneMissionTrigger : MonoBehaviour
     [Header("Mission")]
     public int missionOrder;
 
-    [Header("UI Interact")]
-    public GameObject pressFUI;
-    public KeyCode interactKey = KeyCode.F;
-
     [Header("Scene Text")]
     [TextArea(3, 6)]
     public string sceneText;
@@ -25,61 +21,27 @@ public class SceneMissionTrigger : MonoBehaviour
     public bool useTeleport = true;
     public Transform teleportTarget;
 
-    private bool playerInRange;
     private bool sudahSelesai;
     private Transform playerTransform;
-
-    void Start()
-    {
-        if (pressFUI != null)
-            pressFUI.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (!playerInRange || sudahSelesai)
-            return;
-
-        if (Input.GetKeyDown(interactKey))
-        {
-            bool valid = MissionManager.Instance.ValidateMission(missionOrder);
-            if (!valid) return;
-
-            StartCoroutine(HandleScene());
-        }
-    }
 
     void OnTriggerEnter(Collider other)
     {
         if (sudahSelesai) return;
         if (!other.CompareTag("Player")) return;
 
-        playerInRange = true;
+        if (!MissionManager.Instance.ValidateMission(missionOrder))
+            return;
+
         playerTransform = other.transform;
-
-        if (pressFUI != null)
-            pressFUI.SetActive(true);
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (!other.CompareTag("Player")) return;
-
-        playerInRange = false;
-        playerTransform = null;
-
-        if (pressFUI != null)
-            pressFUI.SetActive(false);
+        StartCoroutine(HandleScene());
     }
 
     IEnumerator HandleScene()
     {
         sudahSelesai = true;
 
-        if (pressFUI != null)
-            pressFUI.SetActive(false);
-
-        PlayerMovement3D controller = playerTransform.GetComponent<PlayerMovement3D>();
+        PlayerMovement3D controller =
+            playerTransform.GetComponent<PlayerMovement3D>();
 
         if (freezePlayer && controller != null)
             controller.Freeze(true);
